354ca4f67c825e8618f4c26482c224e18681c01e
rip <corpusraiden@gmail.com>
2016-05-30 17:00:53 +1000

    Big update

--- usage.js
+++ usage.js
@@ -4,16 +4,17 @@ function toTitleCase(str)
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
-function generate(idname) 
+function generate(idname)
 {
 	var x = "";
-	var s;
-	s = "";
+  var s;
+  s = "";
 
+  // Would like to add the options for prefixes and suffixes.
 	var g = document.getElementById("size").value - 2;		// Size Value
 	var a = document.getElementById("amount").value - 1;	// Amount Value
-	var h = 0;												// Reset for Loop
-	
+  var h = 0;												// Reset for Loop
+
 	while (h <= a)
 	{
 		var k = ["1!","2@","3#","3#","3#","4$","4$","4$","5%"];
@@ -21,11 +22,50 @@ function generate(idname)
 		k = k[Math.floor(Math.random() * k.length)];
 		s = GPW.pronounceable(g);
 		s = toTitleCase(s);
-		x += s + k +"\n";
-	}
-	
+
+    var sym = ["!","@","#","#","#","$","$","$","%"];
+		sym = sym[Math.floor(Math.random() * sym.length)];
+
+    var num = ["1","2","3","3","3","4","4","4","5"];
+		num = num[Math.floor(Math.random() * num.length)];
+
+    if (document.getElementById('symEnable').checked) {
+      // If Symbol and Number Enabled
+      if (document.getElementById('symPosEnd').checked) {
+        x += s + k +"<br />";
+      } else if (document.getElementById('symPosSta').checked) {
+        // Symbol Position Start Requested
+        x += k + s + "<br />";
+      }
+
+    } else if (document.getElementById('symDisable').checked) {
+      // If Symbol and Number Disabled
+      x += s + "<br />";
+
+
+    } else if (document.getElementById('symJust').checked) {
+      if (document.getElementById('symPosEnd').checked) {
+        x += s + sym +"<br />";
+      } else if (document.getElementById('symPosSta').checked) {
+        // Symbol Position Start Requested
+        x += sym + s + "<br />";
+      }
+
+    } else if (document.getElementById('numJust').checked) {
+      if (document.getElementById('symPosEnd').checked) {
+        x += s + num +"<br />";
+      } else if (document.getElementById('symPosSta').checked) {
+        // Symbol Position Start Requested
+        x += num + s + "<br />";
+      }
+    }
+
+
+
+  } // end while
+
 	var v = document.getElementById(idname);
 	v.innerHTML = x;
-	
+
 	return false;
 } // generate
